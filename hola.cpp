b8bc10cd22dcd4ab3002f0ec7b238f91831a9a64
ALIII-37 <britoelena76@gmail.com>
2024-05-01 16:07:58 -0600

    algoritmo
    de 4 caracteres

---
+++ hola.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	
+	int numbe = 4;
+	string hola = "hola";
+	bool verdadero = true;
+	float d = 10.5;
+	
+	
+	
+	cout<<numbe<<endl;
+	cout<<"hola"<<endl;
+	cout<<verdadero<<endl;
+	cout<<d<<endl;
+		
+	return 0;
+}
